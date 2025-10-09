4c62b418124d8fe71541bb5ec014a30ac6106d37
MBATheGamer <mbathegamer@gmail.com>
2025-10-09 16:46:01 +0100

    Add `clear_background` function

--- src/core/draw.rs
+++ src/core/draw.rs
@@ -1,5 +1,16 @@
-use crate::core::ffi::BeginDrawing;
+use crate::{
+  core::ffi::{BeginDrawing, ClearBackground},
+  structs::Color,
+};
 
+#[inline]
+pub fn clear_background(color: Color) {
+  unsafe {
+    ClearBackground(color);
+  }
+}
+
+#[inline]
 pub fn begin_drawing() {
   unsafe {
     BeginDrawing();
--- src/core/ffi.rs
+++ src/core/ffi.rs
@@ -1,9 +1,12 @@
+use crate::structs::Color;
+
 unsafe extern "C" {
   // Window-related functions
   pub unsafe fn InitWindow(width: i32, height: i32, title: *const i8);
   pub unsafe fn WindowShouldClose() -> bool;
 
   // Drawing-related funtions
+  pub unsafe fn ClearBackground(color: Color);
   pub unsafe fn BeginDrawing();
 
   // Timing-related functions
