c08afbf2419c5cca1a996a12a18d385656cd093b
MBATheGamer <mbathegamer@gmail.com>
2025-10-10 20:21:02 +0100

    Complete example

--- src/bin/delta_time.rs
+++ src/bin/delta_time.rs
@@ -1,3 +1,16 @@
+use raylib_ffi::{
+  consts::colors,
+  core::{
+    begin_drawing, clear_background, close_window, end_drawing, get_fps, get_frame_time,
+    init_window, keyboard::is_key_pressed, mouse::get_mouse_wheel_move, set_target_fps,
+    window_should_close,
+  },
+  enums::KeyboardKey,
+  shapes::draw_circle_v,
+  structs::Vector2,
+  text::draw_text,
+};
+
 fn main() {
   let screen_width = 800;
   let screen_height = 450;
@@ -8,14 +21,14 @@ fn main() {
     "raylib [core] example - delta time",
   );
 
-  let current_fps = 60;
+  let mut current_fps = 60;
 
-  let delta_circle = Vector2 {
+  let mut delta_circle = Vector2 {
     x: 0.0,
     y: screen_height as f32 / 3.0,
   };
 
-  let frame_circle = Vector2 {
+  let mut frame_circle = Vector2 {
     x: 0.0,
     y: screen_height as f32 * 2.0 / 3.0,
   };
@@ -28,7 +41,7 @@ fn main() {
   while !window_should_close() {
     let mouse_wheel = get_mouse_wheel_move();
 
-    if mouse_wheel != 0 {
+    if mouse_wheel != 0.0 {
       current_fps += mouse_wheel as i32;
       if current_fps < 0 {
         current_fps = 0;
@@ -39,17 +52,17 @@ fn main() {
     delta_circle.x += get_frame_time() * 6.0 * speed;
     frame_circle.x += 0.1 * speed;
 
-    if delta_circle.x > screen_width {
-      delta_circle.x = 0
+    if delta_circle.x > screen_width as f32 {
+      delta_circle.x = 0.0
     }
 
-    if frame_circle.x > screen_width {
-      frame_circle.x = 0
+    if frame_circle.x > screen_width as f32 {
+      frame_circle.x = 0.0
     }
 
-    if is_key_pressed(Keys::KEY_R) {
-      delta_circle.x = 0;
-      frame_circle.x = 0;
+    if is_key_pressed(KeyboardKey::KeyR) {
+      delta_circle.x = 0.0;
+      frame_circle.x = 0.0;
     }
 
     begin_drawing();
