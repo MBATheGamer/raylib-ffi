2a559402a8d38985a9baa6242878b79db497ffbb
MBATheGamer <mbathegamer@gmail.com>
2025-10-11 18:00:19 +0100

    Add an example

---
+++ src/bin/03_input_keys.rs
@@ -0,0 +1,46 @@
+fn main() {
+  let screen_width = 800;
+  let screen_height = 450;
+
+  init_window(
+    screen_width,
+    screen_height,
+    "raylib [core] example - input keys",
+  );
+
+  let ball_position = Vector2 {
+    x: screen_width as f32 / 2.0,
+    y: screen_height as f32 / 2.0,
+  };
+
+  set_target_fps(60);
+
+  while !window_should_close() {
+    if is_key_down(KeyboardKey::KeyRight) {
+      ball_position.x += 2.0;
+    }
+    if is_key_down(KeyboardKey::KeyLeft) {
+      ball_position.x -= 2.0;
+    }
+    if is_key_down(KeyboardKey::KeyUp) {
+      ball_position.y -= 2.0;
+    }
+    if is_key_down(KeyboardKey::KeyDown) {
+      ball_position.y += 2.0;
+    }
+
+    begin_drawing();
+    clear_background(colors::RAYWHITE);
+    draw_text(
+      "move the ball with arraw keys",
+      10,
+      10,
+      20,
+      colors::DARKGRAY,
+    );
+    draw_circle_v(ball_position, 50.0, colors::MAROON);
+    end_drawing();
+  }
+
+  close_window();
+}
