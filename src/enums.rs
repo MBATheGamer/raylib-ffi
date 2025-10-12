01b21d953216d6764382bc4d05b762bd70ab0160
MBATheGamer <mbathegamer@gmail.com>
2025-10-12 09:24:43 +0100

    Add `MouseButton` enum

--- src/enums.rs
+++ src/enums.rs
@@ -118,3 +118,13 @@ pub enum KeyboardKey {
   KeyVolumeUp = 24,   // Key: Android volume up button
   KeyVolumeDown = 25, // Key: Android volume down button
 }
+
+pub enum MouseButton {
+  Left = 0,    // Mouse button left
+  Right = 1,   // Mouse button right
+  Middle = 2,  // Mouse button middle (pressed wheel)
+  Side = 3,    // Mouse button side (advanced mouse device)
+  Extra = 4,   // Mouse button extra (advanced mouse device)
+  Forward = 5, // Mouse button forward (advanced mouse device)
+  Back = 6,    // Mouse button back (advanced mouse device)
+}
