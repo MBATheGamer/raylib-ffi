69e5b62541ee68145b8b81a81d1f93cc723220a2
MBATheGamer <mbathegamer@gmail.com>
2025-10-13 11:52:25 +0100

    Add `draw_rectangle` function

--- src/shapes/draw/basic.rs
+++ src/shapes/draw/basic.rs
@@ -1,5 +1,5 @@
 use crate::{
-  shapes::ffi::DrawCircleV,
+  shapes::ffi::{DrawCircleV, DrawRectangle},
   structs::{Color, Vector2},
 };
 
@@ -7,5 +7,12 @@ use crate::{
 pub fn draw_circle_v(center: Vector2, radius: f32, color: Color) {
   unsafe {
     DrawCircleV(center, radius, color);
+  }
+}
+
+#[inline]
+pub fn draw_rectangle(pos_x: i32, pos_y: i32, width: i32, height: i32, color: Color) {
+  unsafe {
+    DrawRectangle(pos_x, pos_y, width, height, color);
   }
 }
--- src/shapes/ffi.rs
+++ src/shapes/ffi.rs
@@ -3,4 +3,5 @@ use crate::structs::{Color, Vector2};
 unsafe extern "C" {
   // Basic shapes drawing functions
   pub unsafe fn DrawCircleV(center: Vector2, radius: f32, color: Color);
+  pub unsafe fn DrawRectangle(pos_x: i32, pos_y: i32, width: i32, height: i32, color: Color);
 }
