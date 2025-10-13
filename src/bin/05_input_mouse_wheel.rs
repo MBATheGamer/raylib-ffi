a96c97967332c4fe29530f537a975eaf09548218
MBATheGamer <mbathegamer@gmail.com>
2025-10-13 11:51:40 +0100

    Add an example

---
+++ src/bin/05_input_mouse_wheel.rs
@@ -0,0 +1,50 @@
+fn main() {
+  const SCREEN_WIDTH: i32 = 800;
+  const SCREEN_HEIGHT: i32 = 450;
+
+  init_window(
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    "raylib [core] example - input mouse wheel",
+  );
+
+  let mut box_position_y = SCREEN_HEIGHT / 2 - 40;
+  let scroll_speed = 4;
+
+  set_target_fps(60);
+
+  while !window_should_close() {
+    box_position_y -= (get_mouse_wheel_move() * scroll_speed as f32) as i32;
+
+    begin_drawing();
+
+    clear_background(colors::RAYWHITE);
+
+    draw_rectangle(
+      SCREEN_WIDTH / 2 - 40,
+      box_position_y,
+      80,
+      80,
+      colors::MAROON,
+    );
+
+    draw_text(
+      "Use mouse wheel to move the cube up and down!",
+      10,
+      10,
+      20,
+      colors::GRAY,
+    );
+    draw_text(
+      &format!("Box position Y: {}", box_position_y),
+      10,
+      40,
+      20,
+      colors::LIGHTGRAY,
+    );
+
+    end_drawing();
+  }
+
+  close_window();
+}
