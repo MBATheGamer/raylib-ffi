e5f89905333e03bc45eb7f341d9ecc993cef59eb
MBATheGamer <mbathegamer@gmail.com>
2025-10-14 13:29:30 +0100

    Add `is_gamepad_button_down` function

--- src/core/input/gamepad.rs
+++ src/core/input/gamepad.rs
@@ -1,8 +1,8 @@
 use std::ffi::CStr;
 
 use crate::{
-  core::ffi::{GetGamepadAxisMovement, GetGamepadName, IsGamepadAvailable},
-  enums::GamepadAxis,
+  core::ffi::{GetGamepadAxisMovement, GetGamepadName, IsGamepadAvailable, IsGamepadButtonDown},
+  enums::{GamepadAxis, GamepadButton},
 };
 
 #[inline]
@@ -15,6 +15,11 @@ pub fn get_gamepad_name(gamepad: i32) -> &'static str {
   let c_str = unsafe { CStr::from_ptr(GetGamepadName(gamepad)) };
 
   return c_str.to_str().unwrap();
+}
+
+#[inline]
+pub fn is_gamepad_button_down(gamepad: i32, button: GamepadButton) -> bool {
+  return unsafe { IsGamepadButtonDown(gamepad, button as i32) };
 }
 
 #[inline]
