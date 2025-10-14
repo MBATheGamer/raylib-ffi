f5c18c8ecd59d164152c29c8fde67bba47e717f3
MBATheGamer <mbathegamer@gmail.com>
2025-10-14 12:18:04 +0100

    Add `Texture` and `Texture2D` structs

--- src/structs.rs
+++ src/structs.rs
@@ -13,3 +13,15 @@ pub struct Vector2 {
   pub x: f32,
   pub y: f32,
 }
+
+#[repr(C)]
+#[derive(Clone, Copy, Default)]
+pub struct Texture {
+  pub id: u32,      // OpenGL texture id
+  pub width: i32,   // Texture base width
+  pub height: i32,  // Texture base height
+  pub mipmaps: i32, // Mipmap levels, 1 by default
+  pub format: i32,  // Data format (PixelFormat type)
+}
+
+pub type Texture2D = Texture;
