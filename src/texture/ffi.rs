aa99865211345baa8aa17dc9bb18829b06b79c9f
MBATheGamer <mbathegamer@gmail.com>
2025-10-14 14:43:41 +0100

    Add `unload_texture` function

--- src/texture/ffi.rs
+++ src/texture/ffi.rs
@@ -3,6 +3,7 @@ use crate::structs::{Color, Texture2D};
 unsafe extern "C" {
   // Texture loading functions
   pub unsafe fn LoadTexture(filename: *const i8) -> Texture2D;
+  pub unsafe fn UnloadTexture(texture: Texture2D);
 
   // Texture drawing functions
   pub unsafe fn DrawTexture(texture: Texture2D, pos_x: i32, pos_y: i32, tint: Color);
--- src/texture/loading.rs
+++ src/texture/loading.rs
@@ -1,10 +1,18 @@
 use std::ffi::CString;
 
-use crate::{structs::Texture2D, texture::ffi::LoadTexture};
+use crate::{
+  structs::Texture2D,
+  texture::ffi::{LoadTexture, UnloadTexture},
+};
 
 #[inline]
 pub fn load_texture(filename: &str) -> Texture2D {
   let filename = CString::new(filename).expect("Invalid filename");
 
   return unsafe { LoadTexture(filename.as_ptr()) };
+}
+
+#[inline]
+pub fn unload_texture(texture: Texture2D) {
+  unsafe { UnloadTexture(texture) };
 }
