adfd325ac693204a91b164e6535f4c2964ffaef8
MBATheGamer <mbathegamer@gmail.com>
2025-10-16 09:47:20 +0100

    Add `Gesture` enum

---
+++ src/enums/gesture.rs
@@ -0,0 +1,13 @@
+pub enum Gesture {
+  None = 0,        // No gesture
+  Tap = 1,         // Tap gesture
+  DoubleTap = 2,   // Double tap gesture
+  Hold = 4,        // Hold gesture
+  Drag = 8,        // Drag gesture
+  SwipeRight = 16, // Swipe right gesture
+  SwipeLeft = 32,  // Swipe left gesture
+  SwipeUp = 64,    // Swipe up gesture
+  SwipeDown = 128, // Swipe down gesture
+  PinchIn = 256,   // Pinch in gesture
+  PinchOut = 512,  // Pinch out gesture
+}
--- src/enums/mod.rs
+++ src/enums/mod.rs
@@ -1,11 +1,13 @@
 mod config_flags;
 mod gamepad;
 mod gamepad_button;
+mod gesture;
 mod keyboard_keys;
 mod mouse_button;
 
 pub use config_flags::ConfigFlags;
 pub use gamepad::GamepadAxis;
 pub use gamepad_button::GamepadButton;
+pub use gesture::Gesture;
 pub use keyboard_keys::KeyboardKey;
 pub use mouse_button::MouseButton;
