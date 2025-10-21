30b35f34d2d692515962ddf0b928181972021058
MBATheGamer <mbathegamer@gmail.com>
2025-10-21 13:52:11 +0100

    Add `Camera3D` struct

--- src/structs.rs
+++ src/structs.rs
@@ -61,3 +61,13 @@ pub struct Camera2D {
   pub rotation: f32,   // Camera rotation in degrees
   pub zoom: f32,       // Camera zoom (scaling), should be 1.0f by default
 }
+
+#[repr(C)]
+#[derive(Clone, Copy, Default)]
+pub struct Camera3D {
+  pub position: Vector3, // Camera position
+  pub target: Vector3,   // Camera target it looks-at
+  pub up: Vector3,       // Camera up vector (rotation over its axis)
+  pub fovy: f32, // Camera field-of-view aperture in Y (degrees) in perspective, used as near plane height in world units in orthographic
+  pub projection: i32, // Camera projection: CAMERA_PERSPECTIVE or CAMERA_ORTHOGRAPHIC
+}
