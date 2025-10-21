8c5738174248c57e2cc6ebc0f9305bb0b6e76ca5
MBATheGamer <mbathegamer@gmail.com>
2025-10-21 14:23:43 +0100

    Add `draw_cube_wires` function

--- src/model/draw/basic.rs
+++ src/model/draw/basic.rs
@@ -1,8 +1,12 @@
 use crate::{
-  model::ffi::DrawCube,
+  model::ffi::{DrawCube, DrawCubeWires},
   structs::{Color, Vector3},
 };
 
 pub fn draw_cube(position: Vector3, width: f32, height: f32, length: f32, color: Color) {
   unsafe { DrawCube(position, width, height, length, color) };
+}
+
+pub fn draw_cube_wires(position: Vector3, width: f32, height: f32, length: f32, color: Color) {
+  unsafe { DrawCubeWires(position, width, height, length, color) };
 }
