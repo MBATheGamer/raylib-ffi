1d9cea31b5582b2c4667f8793ba69c5c3c5c1330
MBATheGamer <mbathegamer@gmail.com>
2025-11-01 14:08:18 +0100

    Add an example

---
+++ src/bin/30_scissor_test.rs
@@ -0,0 +1,62 @@
+fn main() {
+  const SCREEN_WIDTH: i32 = 800;
+  const SCREEN_HEIGHT: i32 = 450;
+
+  init_window(
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    "raylib [core] example - scissor test",
+  );
+
+  let scissor_area = Rectangle {
+    x: 0.0,
+    y: 0.0,
+    width: 300.0,
+    height: 300.0,
+  };
+  let scissor_mode = true;
+
+  set_target_fps(60);
+
+  while !window_should_close() {
+    if is_key_pressed(KeyboardKey::KeyS) {
+      scissor_mode = !scissor_mode;
+    }
+
+    scissor_area.x = get_mouse_x() - scissor_area.width / 2;
+    scissor_area.y = get_mouse_y() - scissor_area.height / 2;
+
+    begin_drawing();
+
+    clear_background(colors::RAYWHITE);
+
+    if scissor_mode {
+      begin_scissor_mode(
+        scissor_area.x as i32,
+        scissor_area.y as i32,
+        scissor_area.width as i32,
+        scissor_area.height as i32,
+      );
+    }
+
+    draw_rectangle(0, 0, get_screen_width(), get_screen_height(), colors::RED);
+    draw_text(
+      "Move the mouse around to reveal this text!",
+      190,
+      200,
+      20,
+      colors::LIGHTGRAY,
+    );
+
+    if scissor_mode {
+      end_scissor_mode();
+    }
+
+    draw_rectangle_lines_ex(scissor_area, 1.0, colors::BLACK);
+    draw_text("Press S to toggle scissor test", 10, 10, 20, colors::BLACK);
+
+    end_drawing();
+  }
+
+  close_window();
+}
