0b85b6ce889dc31df8fb00668c20e3b93280fe8a
MBATheGamer <mbathegamer@gmail.com>
2025-11-05 12:58:18 +0100

    Add `draw_text_ex` function

--- src/text/draw.rs
+++ src/text/draw.rs
@@ -1,8 +1,8 @@
 use std::ffi::CString;
 
 use crate::{
-  structs::Color,
-  text::ffi::{DrawFPS, DrawText},
+  structs::{Color, Font, Vector2},
+  text::ffi::{DrawFPS, DrawText, DrawTextEx},
 };
 
 #[inline]
@@ -17,4 +17,18 @@ pub fn draw_text(text: &str, pos_x: i32, pos_y: i32, font_size: i32, color: Colo
   unsafe {
     DrawText(text.as_ptr(), pos_x, pos_y, font_size, color);
   }
+}
+
+#[inline]
+pub fn draw_text_ex(
+  font: Font,
+  text: &str,
+  position: Vector2,
+  font_size: f32,
+  spacing: f32,
+  tint: Color,
+) {
+  let text = CString::new(text).expect("You must set a text in this function");
+
+  unsafe { DrawTextEx(font, text.as_ptr(), position, font_size, spacing, tint) };
 }
--- src/text/ffi.rs
+++ src/text/ffi.rs
@@ -7,6 +7,14 @@ unsafe extern "C" {
   // Text drawing functions
   pub unsafe fn DrawFPS(pos_x: i32, pos_y: i32);
   pub unsafe fn DrawText(text: *const i8, posX: i32, posY: i32, fontSize: i32, color: Color);
+  pub unsafe fn DrawTextEx(
+    font: Font,
+    text: *const i8,
+    position: Vector2,
+    font_size: f32,
+    spacing: f32,
+    tint: Color,
+  );
 
   // Text font info functions
   pub unsafe fn MeasureText(text: *const i8, font_size: i32) -> i32;
