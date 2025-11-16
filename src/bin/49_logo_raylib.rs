f02a6ee76ceb5dd582c30f6c490c6ef65e7bd080
MBATheGamer <mbathegamer@gmail.com>
2025-11-16 18:25:12 +0100

    Add an example

---
+++ src/bin/49_logo_raylib.rs
@@ -0,0 +1,46 @@
+fn main() {
+  const SCREEN_WIDTH: i32 = 800;
+  const SCREEN_HEIGHT: i32 = 450;
+
+  init_window(
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    "raylib [shapes] example - logo raylib",
+  );
+
+  set_target_fps(60);
+
+  while !window_should_close() {
+    begin_drawing();
+
+    clear_background(colors::RAYWHITE);
+
+    draw_rectangle(
+      SCREEN_WIDTH / 2 - 128,
+      SCREEN_HEIGHT / 2 - 128,
+      256,
+      256,
+      colors::BLACK,
+    );
+    draw_rectangle(
+      SCREEN_WIDTH / 2 - 112,
+      SCREEN_HEIGHT / 2 - 112,
+      224,
+      224,
+      colors::RAYWHITE,
+    );
+    draw_text(
+      "raylib",
+      SCREEN_WIDTH / 2 - 44,
+      SCREEN_HEIGHT / 2 + 48,
+      50,
+      colors::BLACK,
+    );
+
+    draw_text("this is NOT a texture!", 350, 370, 10, colors::GRAY);
+
+    end_drawing();
+  }
+
+  close_window();
+}
