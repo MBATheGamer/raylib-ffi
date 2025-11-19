4c6b7c4326a85cad879ccd7df368802bc72cc86a
MBATheGamer <mbathegamer@gmail.com>
2025-11-19 17:26:22 +0100

    Add `check_collision_point_circle` function

--- src/shape/collision.rs
+++ src/shape/collision.rs
@@ -1,9 +1,14 @@
 use crate::{
-  shape::ffi::CheckCollisionPointRec,
+  shape::ffi::{CheckCollisionPointCircle, CheckCollisionPointRec},
   structs::{Rectangle, Vector2},
 };
 
 #[inline]
 pub fn check_collision_point_rec(point: Vector2, rectangle: Rectangle) -> bool {
   return unsafe { CheckCollisionPointRec(point, rectangle) };
+}
+
+#[inline]
+pub fn check_collision_point_circle(point: Vector2, center: Vector2, radius: f32) -> bool {
+  return unsafe { CheckCollisionPointCircle(point, center, radius) };
 }
--- src/shape/ffi.rs
+++ src/shape/ffi.rs
@@ -86,4 +86,5 @@ unsafe extern "C" {
 
   // Basic shapes collision detection functions
   pub unsafe fn CheckCollisionPointRec(point: Vector2, rectangle: Rectangle) -> bool;
+  pub unsafe fn CheckCollisionPointCircle(point: Vector2, center: Vector2, radius: f32) -> bool;
 }
