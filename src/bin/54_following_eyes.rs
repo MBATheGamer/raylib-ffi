9c5b92f40a1bcc8dcf4c59d4efb3ad2ecafbca20
MBATheGamer <mbathegamer@gmail.com>
2025-11-21 08:11:54 +0100

    Complete example

--- src/bin/54_following_eyes.rs
+++ src/bin/54_following_eyes.rs
@@ -1,3 +1,14 @@
+use raylib_ffi::{
+  consts::colors,
+  core::{
+    begin_drawing, clear_background, close_window, end_drawing, get_screen_height,
+    get_screen_width, init_window, mouse::get_mouse_position, set_target_fps, window_should_close,
+  },
+  shape::{check_collision_point_circle, draw_circle_v},
+  structs::Vector2,
+  text::draw_fps,
+};
+
 fn main() {
   const SCREEN_WIDTH: i32 = 800;
   const SCREEN_HEIGHT: i32 = 450;
@@ -18,40 +29,26 @@ fn main() {
   };
   let sclera_radius = 80.0;
 
-  let iris_left_position = Vector2 {
-    x: get_screen_width() as f32 / 2.0 - 100.0,
-    y: get_screen_height() as f32 / 2.0,
-  };
-  let iris_right_position = Vector2 {
-    x: get_screen_width() as f32 / 2.0 + 100.0,
-    y: get_screen_height() as f32 / 2.0,
-  };
   let iris_radius = 24;
-
-  let angle = 0.0;
-  let dx = 0.0;
-  let dy = 0.0;
-  let dxx = 0.0;
-  let dyy = 0.0;
 
   set_target_fps(60);
 
   while !window_should_close() {
-    iris_left_position = get_mouse_position();
-    iris_right_position = get_mouse_position();
+    let mut iris_left_position = get_mouse_position();
+    let mut iris_right_position = get_mouse_position();
 
     if !check_collision_point_circle(
       iris_left_position,
       sclera_left_position,
       sclera_radius - iris_radius as f32,
     ) {
-      dx = iris_left_position.x - sclera_left_position.x;
-      dy = iris_left_position.y - sclera_left_position.y;
+      let dx = iris_left_position.x - sclera_left_position.x;
+      let dy = iris_left_position.y - sclera_left_position.y;
 
-      angle = dy.atan2(dx);
+      let angle = dy.atan2(dx);
 
-      dxx = (sclera_radius - iris_radius as f32) * angle.cos();
-      dyy = (sclera_radius - iris_radius as f32) * angle.sin();
+      let dxx = (sclera_radius - iris_radius as f32) * angle.cos();
+      let dyy = (sclera_radius - iris_radius as f32) * angle.sin();
 
       iris_left_position.x = sclera_left_position.x + dxx;
       iris_left_position.y = sclera_left_position.y + dyy;
@@ -62,13 +59,13 @@ fn main() {
       sclera_right_position,
       sclera_radius - iris_radius as f32,
     ) {
-      dx = iris_right_position.x - sclera_right_position.x;
-      dy = iris_right_position.y - sclera_right_position.y;
+      let dx = iris_right_position.x - sclera_right_position.x;
+      let dy = iris_right_position.y - sclera_right_position.y;
 
-      angle = dy.atan2(dx);
+      let angle = dy.atan2(dx);
 
-      dxx = (sclera_radius - iris_radius as f32) * angle.cos();
-      dyy = (sclera_radius - iris_radius as f32) * angle.sin();
+      let dxx = (sclera_radius - iris_radius as f32) * angle.cos();
+      let dyy = (sclera_radius - iris_radius as f32) * angle.sin();
 
       iris_right_position.x = sclera_right_position.x + dxx;
       iris_right_position.y = sclera_right_position.y + dyy;
