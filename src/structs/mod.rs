db769ed63ab6bc4615f4085f7efc9565ce0c416c
MBATheGamer <mbathegamer@gmail.com>
2025-11-24 08:15:50 +0100

    Refactor: `Matrix` struct

--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -1,33 +1,14 @@
 use std::ffi::c_void;
 
+mod matrix;
 mod vector2;
 mod vector3;
 
+pub use matrix::Matrix;
 pub use vector2::Vector2;
 pub use vector3::Vector3;
 
 use crate::enums::CameraProjection;
-
-#[repr(C)]
-#[derive(Clone, Copy, Default)]
-pub struct Matrix {
-  pub m0: f32,
-  pub m4: f32,
-  pub m8: f32,
-  pub m12: f32, // Matrix first row (4 components)
-  pub m1: f32,
-  pub m5: f32,
-  pub m9: f32,
-  pub m13: f32, // Matrix second row (4 components)
-  pub m2: f32,
-  pub m6: f32,
-  pub m10: f32,
-  pub m14: f32, // Matrix third row (4 components)
-  pub m3: f32,
-  pub m7: f32,
-  pub m11: f32,
-  pub m15: f32, // Matrix fourth row (4 components)
-}
 
 #[repr(C)]
 #[derive(Clone, Copy, Default, PartialEq)]
