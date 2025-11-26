afbe4c6b2f597fb34805c0530ee3527fb9ad3812
MBATheGamer <mbathegamer@gmail.com>
2025-11-26 13:14:38 +0100

    Add `Windows/MinGW` platform to build file

--- build.rs
+++ build.rs
@@ -1,6 +1,14 @@
 use std::env;
 
 #[inline]
+fn windows_gnu_lib(lib_name: &str) {
+  println!("cargo:rustc-link-lib={}", lib_name);
+  println!("cargo:rustc-link-lib=shell32");
+  println!("cargo:rustc-link-lib=gdi32");
+  println!("cargo:rustc-link-lib=winmm");
+}
+
+#[inline]
 fn windows_msvc_lib(lib_name: &str) {
   println!("cargo:rustc-link-lib={}_msvc", lib_name);
   println!("cargo:rustc-link-lib=shell32");
@@ -16,8 +24,10 @@ fn main() {
   let mut platform = "DESKTOP";
 
   match target.as_str() {
+    "i686-pc-windows-gnu" => windows_gnu_lib("raylib_w32"),
     "i686-pc-windows-msvc" => windows_msvc_lib("raylib_w32"),
     "i686-unknown-linux-gnu" => println!("cargo:rustc-link-lib=raylib_x86"),
+    "x86_64-pc-windows-gnu" => windows_gnu_lib("raylib_w64"),
     "x86_64-pc-windows-msvc" => windows_msvc_lib("raylib_w64"),
     "x86_64-unknown-linux-gnu" => println!("cargo:rustc-link-lib=raylib_x64"),
     "wasm32-unknown-unknown" => {
