df2550c223c1e7add8c774fd13a29ccf6a5ec518
MBATheGamer <mbathegamer@gmail.com>
2025-11-26 12:53:52 +0100

    Add `PLATFORM` enviroment variable to build file

--- build.rs
+++ build.rs
@@ -4,12 +4,16 @@ fn main() {
   println!("cargo:rustc-link-search=./lib");
 
   let target = env::var("TARGET").expect("TARGET environment variable not set.");
+  let mut platform = "DESKTOP";
 
   match target.as_str() {
     "x86_64-unknown-linux-gnu" => println!("cargo:rustc-link-lib=raylib_x64"),
     "wasm32-unknown-unknown" => {
       println!("cargo:rustc-link-lib=raylib_wasm");
+      platform = "WEB";
     }
     _ => println!("cargo:warning=Unsupported target architecture"),
   };
+
+  println!("cargo:rustc-env=PLATFORM={}", platform);
 }
