a5df3d16cf5d4394f28246d4d4695929ab5a2824
MBATheGamer <mbathegamer@gmail.com>
2025-11-27 10:08:07 +0100

    Add an example

---
+++ src/bin/81_srcrec_dstrec.rs
@@ -0,0 +1,84 @@
+fn main() {
+  const SCREEN_WIDTH: i32 = 800;
+  const SCREEN_HEIGHT: i32 = 450;
+
+  init_window(
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    "raylib [textures] example - srcrec dstrec",
+  );
+
+  let scarfy = load_texture("resources/scarfy.png");
+
+  let frame_width = scarfy.width / 6;
+  let frame_height = scarfy.height;
+
+  let source_rec = Rectangle {
+    x: 0.0,
+    y: 0.0,
+    width: frame_width as f32,
+    height: frame_height as f32,
+  };
+
+  let dest_rec = Rectangle {
+    x: SCREEN_WIDTH as f32 / 2.0,
+    y: SCREEN_HEIGHT as f32 / 2.0,
+    width: frame_width as f32 * 2.0,
+    height: frame_height as f32 * 2.0,
+  };
+
+  let origin = Vector2 {
+    x: frame_width as f32,
+    y: frame_height as f32,
+  };
+
+  let mut rotation = 0;
+
+  set_target_fps(60);
+
+  while !window_should_close() {
+    rotation += 1;
+
+    begin_drawing();
+
+    clear_background(colors::RAYWHITE);
+
+    draw_texture_pro(
+      scarfy,
+      source_rec,
+      dest_rec,
+      origin,
+      rotation as f32,
+      colors::WHITE,
+    );
+
+    draw_line(
+      dest_rec.x as i32,
+      0,
+      dest_rec.x as i32,
+      SCREEN_HEIGHT,
+      colors::GRAY,
+    );
+    draw_line(
+      0,
+      dest_rec.y as i32,
+      SCREEN_WIDTH,
+      dest_rec.y as i32,
+      colors::GRAY,
+    );
+
+    draw_text(
+      "(c) Scarfy sprite by Eiden Marsal",
+      SCREEN_WIDTH - 200,
+      SCREEN_HEIGHT - 20,
+      10,
+      colors::GRAY,
+    );
+
+    end_drawing();
+  }
+
+  unload_texture(scarfy);
+
+  close_window();
+}
