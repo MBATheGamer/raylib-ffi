f45ecfaad49ee64cff1d90d5299d781202e869d6
MBATheGamer <mbathegamer@gmail.com>
2025-11-27 10:08:19 +0100

    Complete example

--- src/bin/81_srcrec_dstrec.rs
+++ src/bin/81_srcrec_dstrec.rs
@@ -1,3 +1,15 @@
+use raylib_ffi::{
+  consts::colors,
+  core::{
+    begin_drawing, clear_background, close_window, end_drawing, init_window, set_target_fps,
+    window_should_close,
+  },
+  shape::draw_line,
+  structs::{Rectangle, Vector2},
+  text::draw_text,
+  texture::{draw_texture_pro, load_texture, unload_texture},
+};
+
 fn main() {
   const SCREEN_WIDTH: i32 = 800;
   const SCREEN_HEIGHT: i32 = 450;
