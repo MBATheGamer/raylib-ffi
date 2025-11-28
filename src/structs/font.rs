1e5ebb07f491a3fc9e06176867530704f21cb3f6
MBATheGamer <mbathegamer@gmail.com>
2025-11-28 09:08:23 +0100

    Refactor: Make `Font` struct fields public

--- src/structs/font.rs
+++ src/structs/font.rs
@@ -1,10 +1,10 @@
 #[repr(C)]
 #[derive(Clone, Copy, Default)]
 pub struct Font {
-  base_size: i32,                // Base size (default chars height)
-  glyph_count: i32,              // Number of glyph characters
-  glyph_padding: i32,            // Padding around the glyph characters
-  texture: super::Texture,       // Texture atlas containing the glyphs
-  recs: *mut super::Rectangle,   // Rectangles in texture for the glyphs
-  glyphs: *mut super::GlyphInfo, // Glyphs info data
+  pub base_size: i32,                // Base size (default chars height)
+  pub glyph_count: i32,              // Number of glyph characters
+  pub glyph_padding: i32,            // Padding around the glyph characters
+  pub texture: super::Texture,       // Texture atlas containing the glyphs
+  pub recs: *mut super::Rectangle,   // Rectangles in texture for the glyphs
+  pub glyphs: *mut super::GlyphInfo, // Glyphs info data
 }
