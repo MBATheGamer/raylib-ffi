85a86831f9ea3f3e4ad80d81292cf2f1f22b07c7
MBATheGamer <mbathegamer@gmail.com>
2025-11-28 08:39:17 +0100

    Refactor: Make `Image` struct fields public

--- src/structs/image.rs
+++ src/structs/image.rs
@@ -3,9 +3,9 @@ use std::ffi::c_void;
 #[repr(C)]
 #[derive(Clone, Copy, Default)]
 pub struct Image {
-  data: *mut c_void, // Image raw data
-  width: i32,        // Image base width
-  height: i32,       // Image base height
-  mipmaps: i32,      // Mipmap levels, 1 by default
-  format: i32,       // Data format (PixelFormat type)
+  pub data: *mut c_void, // Image raw data
+  pub width: i32,        // Image base width
+  pub height: i32,       // Image base height
+  pub mipmaps: i32,      // Mipmap levels, 1 by default
+  pub format: i32,       // Data format (PixelFormat type)
 }
