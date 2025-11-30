ac7c13d866582a8e3ea1e2e1880d5c0c3b9b0416
MBATheGamer <mbathegamer@gmail.com>
2025-11-30 09:31:14 +0100

    Add an example

---
+++ src/bin/84_image_loading.rs
@@ -0,0 +1,43 @@
+fn main() {
+  const SCREEN_WIDTH: i32 = 800;
+  const SCREEN_HEIGHT: i32 = 450;
+
+  init_window(
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    "raylib [textures] example - image loading",
+  );
+
+  let image = load_image("resources/raylib_logo.png");
+  let texture = load_texture_from_image(image);
+  unload_image(image);
+
+  set_target_fps(60);
+
+  while !window_should_close() {
+    begin_drawing();
+
+    clear_background(colors::RAYWHITE);
+
+    draw_texture(
+      texture,
+      SCREEN_WIDTH / 2 - texture.width / 2,
+      SCREEN_HEIGHT / 2 - texture.height / 2,
+      colors::WHITE,
+    );
+
+    draw_text(
+      "this IS a texture loaded from an image!",
+      300,
+      370,
+      10,
+      colors::GRAY,
+    );
+
+    end_drawing();
+  }
+
+  unload_texture(texture);
+
+  close_window();
+}
