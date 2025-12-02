cc334af4e61bfcdf898995e3e0542c2d02853574
MBATheGamer <mbathegamer@gmail.com>
2025-12-02 12:33:55 +0100

    Complete example

--- src/bin/86_image_text.rs
+++ src/bin/86_image_text.rs
@@ -1,3 +1,18 @@
+use raylib_ffi::{
+  consts::colors,
+  core::{
+    begin_drawing, clear_background, close_window, end_drawing, init_window, keyboard::is_key_down,
+    set_target_fps, window_should_close,
+  },
+  enums::KeyboardKey,
+  structs::Vector2,
+  text::{draw_text, draw_text_ex, load_font_ex, unload_font},
+  texture::{
+    draw_texture, draw_texture_v, image_draw_text_ex, load_image, load_texture_from_image,
+    unload_image, unload_texture,
+  },
+};
+
 fn main() {
   const SCREEN_WIDTH: i32 = 800;
   const SCREEN_HEIGHT: i32 = 450;
@@ -8,16 +23,16 @@ fn main() {
     "raylib [textures] example - image text",
   );
 
-  let parrots = load_image("resources/parrots.png");
+  let mut parrots = load_image("resources/parrots.png");
 
-  let font = load_font_ex("resources/KAISG.ttf", 64, 0, 0);
+  let font = load_font_ex("resources/KAISG.ttf", 64, None, 0);
 
   image_draw_text_ex(
     &mut parrots,
     font,
     "[Parrots font drawing]",
     Vector2 { x: 20.0, y: 20.0 },
-    font.baseSize as f32,
+    font.base_size as f32,
     0.0,
     colors::RED,
   );
@@ -30,7 +45,7 @@ fn main() {
     y: (SCREEN_HEIGHT / 2 - texture.height / 2 - 20) as f32,
   };
 
-  let show_font = false;
+  let mut show_font;
 
   set_target_fps(60);
 
@@ -55,7 +70,7 @@ fn main() {
           x: position.x + 20.0,
           y: position.y + 20.0 + 280.0,
         },
-        font.baseSize as f32,
+        font.base_size as f32,
         0.0,
         colors::WHITE,
       );
