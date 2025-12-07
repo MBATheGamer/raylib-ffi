6575ccb86e33eb9bf42eefd2816ad0d0faeadafb
MBATheGamer <mbathegamer@gmail.com>
2025-12-07 11:37:12 +0100

    Add `get_color` function

--- src/texture/pixel.rs
+++ src/texture/pixel.rs
@@ -1,6 +1,6 @@
 use crate::{
   structs::Color,
-  texture::ffi::{ColorAlpha, Fade},
+  texture::ffi::{ColorAlpha, Fade, GetColor},
 };
 
 #[inline]
@@ -11,4 +11,9 @@ pub fn fade(color: Color, alpha: f32) -> Color {
 #[inline]
 pub fn color_alpha(color: Color, alpha: f32) -> Color {
   return unsafe { ColorAlpha(color, alpha) };
+}
+
+#[inline]
+pub fn get_color(hex_value: u32) -> Color {
+  return unsafe { GetColor(hex_value) };
 }
