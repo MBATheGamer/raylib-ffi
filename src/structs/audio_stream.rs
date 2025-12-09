2ae3ce881e92b39d1cfcb219ec11ff83f6b20130
MBATheGamer <mbathegamer@gmail.com>
2025-12-09 12:06:17 +0100

    Fix: warning: extern block uses type Y, which is not FFI-safe

--- src/structs/audio_stream.rs
+++ src/structs/audio_stream.rs
@@ -1,10 +1,15 @@
 #[repr(C)]
-struct AudioBuffer;
+struct AudioBuffer {
+  _private: [u8; 0],
+}
 
 #[repr(C)]
-struct AudioProcessor;
+struct AudioProcessor {
+  _private: [u8; 0],
+}
 
 #[repr(C)]
+#[derive(Clone, Copy)]
 pub struct AudioStream {
   buffer: *mut AudioBuffer, // Pointer to internal data used by the audio system
   processor: *mut AudioProcessor, // Pointer to internal data processor, useful for audio effects
--- src/structs/sound.rs
+++ src/structs/sound.rs
@@ -1,6 +1,7 @@
 use crate::structs::AudioStream;
 
 #[repr(C)]
+#[derive(Clone, Copy)]
 pub struct Sound {
   stream: AudioStream, // Audio stream
   frame_count: u32,    // Total number of frames (considering channels)
