cd84c756c3653a13cc50b761d2fab980d39ad4de
MBATheGamer <mbathegamer@gmail.com>
2025-12-11 15:43:35 +0100

    Add `-` operator to `Vector2` struct

--- src/structs/vector2.rs
+++ src/structs/vector2.rs
@@ -1,6 +1,28 @@
+use std::ops::Sub;
+
 #[repr(C)]
 #[derive(Clone, Copy, Default)]
 pub struct Vector2 {
   pub x: f32,
   pub y: f32,
+}
+
+impl Vector2 {
+  pub fn sub(&self, rhs: Vector2) -> Vector2 {
+    return Vector2 {
+      x: self.x - rhs.x,
+      y: self.y - rhs.y,
+    };
+  }
+}
+
+impl Sub for Vector2 {
+  type Output = Vector2;
+
+  fn sub(self, rhs: Vector2) -> Vector2 {
+    return Vector2 {
+      x: self.x - rhs.x,
+      y: self.y - rhs.y,
+    };
+  }
 }
