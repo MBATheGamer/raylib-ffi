75a69c979032a54e013f2aa3e4743a63a2ab6728
MBATheGamer <mbathegamer@gmail.com>
2025-12-12 14:22:35 +0100

    Add `length` method to `Vector3` function

--- src/structs/vector3.rs
+++ src/structs/vector3.rs
@@ -7,6 +7,10 @@ pub struct Vector3 {
 }
 
 impl Vector3 {
+  pub fn length(&self) -> f32 {
+    return self.x * self.x + self.y * self.y + self.z * self.z;
+  }
+
   pub fn lerp(&self, other_vector: Vector3, amount: f32) -> Vector3 {
     return Vector3 {
       x: self.x + amount * (other_vector.x - self.x),
