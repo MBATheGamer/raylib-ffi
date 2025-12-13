caef25e1369e418f6d575496411bf79ca70d3921
MBATheGamer <mbathegamer@gmail.com>
2025-12-13 14:07:20 +0100

    Add `rl_pop_matrix` function

--- src/rlgl/ffi.rs
+++ src/rlgl/ffi.rs
@@ -1,5 +1,6 @@
 unsafe extern "C" {
   pub unsafe fn rlPushMatrix();
+  pub unsafe fn rlPopMatrix();
   pub unsafe fn rlRotatef(angle: f32, x: f32, y: f32, z: f32);
   pub unsafe fn rlTranslatef(x: f32, y: f32, z: f32);
 }
--- src/rlgl/mod.rs
+++ src/rlgl/mod.rs
@@ -1,9 +1,13 @@
-use crate::rlgl::ffi::{rlPushMatrix, rlRotatef, rlTranslatef};
+use crate::rlgl::ffi::{rlPopMatrix, rlPushMatrix, rlRotatef, rlTranslatef};
 
 mod ffi;
 
 pub fn rl_push_matrix() {
   unsafe { rlPushMatrix() };
+}
+
+pub fn rl_pop_matrix() {
+  unsafe { rlPopMatrix() };
 }
 
 pub fn rl_rotate(angle: f32, x: f32, y: f32, z: f32) {
