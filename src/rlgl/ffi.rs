cabcf5d7306551b7ccddfcbdba12b50e7e75b6b5
MBATheGamer <mbathegamer@gmail.com>
2025-12-13 14:06:31 +0100

    Add `rl_rotate` function

--- src/rlgl/ffi.rs
+++ src/rlgl/ffi.rs
@@ -1,4 +1,5 @@
 unsafe extern "C" {
   pub unsafe fn rlPushMatrix();
+  pub unsafe fn rlRotatef(angle: f32, x: f32, y: f32, z: f32);
   pub unsafe fn rlTranslatef(x: f32, y: f32, z: f32);
 }
--- src/rlgl/mod.rs
+++ src/rlgl/mod.rs
@@ -1,9 +1,13 @@
-use crate::rlgl::ffi::{rlPushMatrix, rlTranslatef};
+use crate::rlgl::ffi::{rlPushMatrix, rlRotatef, rlTranslatef};
 
 mod ffi;
 
 pub fn rl_push_matrix() {
   unsafe { rlPushMatrix() };
+}
+
+pub fn rl_rotate(angle: f32, x: f32, y: f32, z: f32) {
+  unsafe { rlRotatef(angle, x, y, z) };
 }
 
 pub fn rl_translate(x: f32, y: f32, z: f32) {
