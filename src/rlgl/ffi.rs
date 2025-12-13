ba39b54616b57caf4876f234ef23c5f12d8e4b53
MBATheGamer <mbathegamer@gmail.com>
2025-12-13 14:04:10 +0100

    Add `rl_translate` function

--- src/rlgl/ffi.rs
+++ src/rlgl/ffi.rs
@@ -1,3 +1,4 @@
 unsafe extern "C" {
   pub unsafe fn rlPushMatrix();
+  pub unsafe fn rlTranslatef(x: f32, y: f32, z: f32);
 }
--- src/rlgl/mod.rs
+++ src/rlgl/mod.rs
@@ -1,7 +1,11 @@
-use crate::rlgl::ffi::rlPushMatrix;
+use crate::rlgl::ffi::{rlPushMatrix, rlTranslatef};
 
 mod ffi;
 
 pub fn rl_push_matrix() {
   unsafe { rlPushMatrix() };
+}
+
+pub fn rl_translate(x: f32, y: f32, z: f32) {
+  unsafe { rlTranslatef(x, y, z) };
 }
