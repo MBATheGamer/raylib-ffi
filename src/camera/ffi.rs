9c98e520fc1f89b413578fe74edda38f21dbbfce
MBATheGamer <mbathegamer@gmail.com>
2025-12-14 07:33:43 +0100

    Add `camera_pitch` method to `Camera3D` struct

--- src/camera/ffi.rs
+++ src/camera/ffi.rs
@@ -2,4 +2,11 @@ use crate::structs::Camera3D;
 
 unsafe extern "C" {
   pub fn CameraYaw(camera: *mut Camera3D, angle: f32, rotate_around_target: bool);
+  pub fn CameraPitch(
+    camera: *mut Camera3D,
+    angle: f32,
+    lock_view: bool,
+    rotate_around_target: bool,
+    rotate_up: bool,
+  );
 }
--- src/camera/mod.rs
+++ src/camera/mod.rs
@@ -1,4 +1,7 @@
-use crate::{camera::ffi::CameraYaw, structs::Camera3D};
+use crate::{
+  camera::ffi::{CameraPitch, CameraYaw},
+  structs::Camera3D,
+};
 
 mod ffi;
 
@@ -6,5 +9,16 @@ impl Camera3D {
   #[inline]
   pub fn camera_yaw(&mut self, angle: f32, rotate_around_target: bool) {
     unsafe { CameraYaw(self, angle, rotate_around_target) };
+  }
+
+  #[inline]
+  pub fn camera_pitch(
+    &mut self,
+    angle: f32,
+    lock_view: bool,
+    rotate_around_target: bool,
+    rotate_up: bool,
+  ) {
+    unsafe { CameraPitch(self, angle, lock_view, rotate_around_target, rotate_up) };
   }
 }
