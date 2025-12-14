5bdbad1b5a0a55aaf4cbff1c6175d6d37f0e802a
MBATheGamer <mbathegamer@gmail.com>
2025-12-14 08:30:03 +0100

    Add `normalize` function

--- src/math.rs
+++ src/math.rs
@@ -1,3 +1,7 @@
 pub fn lerp(start: f32, end: f32, amount: f32) -> f32 {
   return start + amount * (end - start);
 }
+
+pub fn normalize(value: f32, start: f32, end: f32) -> f32 {
+  return (value - start) / (end - start);
+}
