0b353e45fc260a49abbe6a5c25737d778c4ed107
MBATheGamer <mbathegamer@gmail.com>
2025-12-14 08:30:26 +0100

    Add `remap` function

--- src/math.rs
+++ src/math.rs
@@ -5,3 +5,14 @@ pub fn lerp(start: f32, end: f32, amount: f32) -> f32 {
 pub fn normalize(value: f32, start: f32, end: f32) -> f32 {
   return (value - start) / (end - start);
 }
+
+pub fn remap(
+  value: f32,
+  input_start: f32,
+  input_end: f32,
+  output_start: f32,
+  output_end: f32,
+) -> f32 {
+  return (value - input_start) / (input_end - input_start) * (output_end - output_start)
+    + output_start;
+}
