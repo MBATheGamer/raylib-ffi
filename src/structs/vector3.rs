cd86d0d8edae0a3573841369879666f8cbb84a06
MBATheGamer <mbathegamer@gmail.com>
2025-12-15 13:53:58 +0100

    Add `sub_value` method to `Vector3` struct

--- src/structs/vector3.rs
+++ src/structs/vector3.rs
@@ -55,6 +55,15 @@ impl Vector3 {
   }
 
   #[inline]
+  pub fn sub_value(&self, add: f32) -> Vector3 {
+    return Vector3 {
+      x: self.x - add,
+      y: self.y - add,
+      z: self.z - add,
+    };
+  }
+
+  #[inline]
   pub fn angle(&self, other: Vector3) -> f32 {
     let cross = Vector3 {
       x: self.y * other.z - self.z * other.y,
