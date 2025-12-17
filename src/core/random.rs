17734da7945f480e78d7ed6d432e4b4bd019f5cf
MBATheGamer <mbathegamer@gmail.com>
2025-12-17 18:02:05 +0100

    Add `load_random_sequence` function

--- src/core/random.rs
+++ src/core/random.rs
@@ -1,6 +1,13 @@
-use crate::core::ffi::GetRandomValue;
+use crate::core::ffi::{GetRandomValue, LoadRandomSequence};
 
 #[inline]
 pub fn get_random_value(min: i32, max: i32) -> i32 {
   return unsafe { GetRandomValue(min, max) };
+}
+
+#[inline]
+pub fn load_random_sequence(count: usize, min: i32, max: i32) -> Vec<i32> {
+  let ptr = unsafe { LoadRandomSequence(count as u32, min, max) };
+
+  return unsafe { Vec::from_raw_parts(ptr, count, count + 1) };
 }
