927ec64ade616272140103d3e6a5faa8c3992ed1
MBATheGamer <mbathegamer@gmail.com>
2025-12-18 14:19:58 +0100

    Complete example

--- src/bin/40_viewport_scaling.rs
+++ src/bin/40_viewport_scaling.rs
@@ -1,3 +1,18 @@
+use raylib_ffi::{
+  consts::colors,
+  core::{
+    begin_drawing, begin_texture_mode, clear_background, close_window, end_drawing,
+    end_texture_mode, get_screen_height, get_screen_width, init_window, is_window_resized,
+    mouse::{get_mouse_position, is_mouse_button_pressed},
+    set_config_flags, set_target_fps, window_should_close,
+  },
+  enums::{ConfigFlags, MouseButton},
+  shape::{check_collision_point_rec, draw_circle, draw_rectangle_lines, draw_rectangle_rec},
+  structs::{Rectangle, RenderTexture, Vector2},
+  text::draw_text,
+  texture::{draw_texture_pro, fade, load_render_texture, unload_render_texture},
+};
+
 const RESOLUTION_COUNT: usize = 4;
 const VIEWPORT_TYPE_COUNT: usize = 6;
 
@@ -32,16 +47,16 @@ fn main() {
       y: 2160.0,
     },
   ];
-  let resolution_index = 0;
+  let mut resolution_index = 0;
 
-  let screen_width = 800;
-  let screen_height = 450;
-  let game_width = 64;
-  let game_height = 64;
+  let mut screen_width = 800;
+  let mut screen_height = 450;
+  let mut game_width = 64;
+  let mut game_height = 64;
 
-  let target = RenderTexture::default();
-  let source_rect = Rectangle::default();
-  let dest_rect = Rectangle::default();
+  let mut target = RenderTexture::default();
+  let mut source_rect = Rectangle::default();
+  let mut dest_rect = Rectangle::default();
 
   let viewport_type_names: Vec<&str> = vec![
     "KEEP_ASPECT_INTEGER",
@@ -51,7 +66,7 @@ fn main() {
     "KEEP_HEIGHT",
     "KEEP_WIDTH",
   ];
-  let viewport_type = ViewportType::KeepAspectInteger;
+  let mut viewport_type = ViewportType::KeepAspectInteger;
 
   set_config_flags(&[ConfigFlags::WindowResizable]);
   init_window(
@@ -61,13 +76,13 @@ fn main() {
   );
   resize_render_size(
     viewport_type,
-    &screen_width,
-    &screen_height,
+    &mut screen_width,
+    &mut screen_height,
     game_width,
     game_height,
-    &source_rect,
-    &dest_rect,
-    &target,
+    &mut source_rect,
+    &mut dest_rect,
+    &mut target,
   );
 
   set_target_fps(60);
@@ -101,13 +116,13 @@ fn main() {
     if is_window_resized() {
       resize_render_size(
         viewport_type,
-        &screen_width,
-        &screen_height,
+        &mut screen_width,
+        &mut screen_height,
         game_width,
         game_height,
-        &source_rect,
-        &dest_rect,
-        &target,
+        &mut source_rect,
+        &mut dest_rect,
+        &mut target,
       );
     }
     let mouse_position = get_mouse_position();
@@ -119,13 +134,13 @@ fn main() {
       game_height = resolution_list[resolution_index].y as i32;
       resize_render_size(
         viewport_type,
-        &screen_width,
-        &screen_height,
+        &mut screen_width,
+        &mut screen_height,
         game_width,
         game_height,
-        &source_rect,
-        &dest_rect,
-        &target,
+        &mut source_rect,
+        &mut dest_rect,
+        &mut target,
       );
     }
     if check_collision_point_rec(mouse_position, increase_resolution_button) && mouse_pressed {
@@ -134,13 +149,13 @@ fn main() {
       game_height = resolution_list[resolution_index].y as i32;
       resize_render_size(
         viewport_type,
-        &screen_width,
-        &screen_height,
+        &mut screen_width,
+        &mut screen_height,
         game_width,
         game_height,
-        &source_rect,
-        &dest_rect,
-        &target,
+        &mut source_rect,
+        &mut dest_rect,
+        &mut target,
       );
     }
     if check_collision_point_rec(mouse_position, decrease_type_button) && mouse_pressed {
@@ -148,26 +163,26 @@ fn main() {
         get((viewport_type as i32 + VIEWPORT_TYPE_COUNT as i32 - 1) % VIEWPORT_TYPE_COUNT as i32);
       resize_render_size(
         viewport_type,
-        &screen_width,
-        &screen_height,
+        &mut screen_width,
+        &mut screen_height,
         game_width,
         game_height,
-        &source_rect,
-        &dest_rect,
-        &target,
+        &mut source_rect,
+        &mut dest_rect,
+        &mut target,
       );
     }
     if check_collision_point_rec(mouse_position, increase_type_button) && mouse_pressed {
       viewport_type = get((viewport_type as i32 + 1) % VIEWPORT_TYPE_COUNT as i32);
       resize_render_size(
         viewport_type,
-        &screen_width,
-        &screen_height,
+        &mut screen_width,
+        &mut screen_height,
         game_width,
         game_height,
-        &source_rect,
-        &dest_rect,
-        &target,
+        &mut source_rect,
+        &mut dest_rect,
+        &mut target,
       );
     }
 
@@ -316,8 +331,8 @@ fn keep_aspect_centered_integer(
   screen_height: i32,
   game_width: i32,
   game_height: i32,
-  source_rect: &Rectangle,
-  dest_rect: &Rectangle,
+  source_rect: &mut Rectangle,
+  dest_rect: &mut Rectangle,
 ) {
   source_rect.x = 0.;
   source_rect.y = game_height as f32;
@@ -339,8 +354,8 @@ fn keep_height_centered_integer(
   screen_height: i32,
   _: i32,
   game_height: i32,
-  source_rect: &Rectangle,
-  dest_rect: &Rectangle,
+  source_rect: &mut Rectangle,
+  dest_rect: &mut Rectangle,
 ) {
   let resize_ratio = (screen_height / game_height) as f32;
   source_rect.x = 0.0;
@@ -359,8 +374,8 @@ fn keep_width_centered_integer(
   screen_height: i32,
   game_width: i32,
   _: i32,
-  source_rect: &Rectangle,
-  dest_rect: &Rectangle,
+  source_rect: &mut Rectangle,
+  dest_rect: &mut Rectangle,
 ) {
   let resize_ratio = (screen_width / game_width) as f32;
   source_rect.x = 0.0;
@@ -381,8 +396,8 @@ fn keep_aspect_centered(
   screen_height: i32,
   game_width: i32,
   game_height: i32,
-  source_rect: &Rectangle,
-  dest_rect: &Rectangle,
+  source_rect: &mut Rectangle,
+  dest_rect: &mut Rectangle,
 ) {
   source_rect.x = 0.0;
   source_rect.y = game_height as f32;
@@ -404,8 +419,8 @@ fn keep_height_centered(
   screen_height: i32,
   _: i32,
   game_height: i32,
-  source_rect: &Rectangle,
-  dest_rect: &Rectangle,
+  source_rect: &mut Rectangle,
+  dest_rect: &mut Rectangle,
 ) {
   let resize_ratio = screen_height as f32 / game_height as f32;
   source_rect.x = 0.0;
@@ -424,8 +439,8 @@ fn keep_width_centered(
   screen_height: i32,
   game_width: i32,
   _: i32,
-  source_rect: &Rectangle,
-  dest_rect: &Rectangle,
+  source_rect: &mut Rectangle,
+  dest_rect: &mut Rectangle,
 ) {
   let resize_ratio = screen_width as f32 / game_width as f32;
   source_rect.x = 0.0;
@@ -443,13 +458,13 @@ fn keep_width_centered(
 
 fn resize_render_size(
   viewport_type: ViewportType,
-  screen_width: &i32,
-  screen_height: &i32,
+  screen_width: &mut i32,
+  screen_height: &mut i32,
   game_width: i32,
   game_height: i32,
-  source_rect: &Rectangle,
-  dest_rect: &Rectangle,
-  target: &RenderTexture,
+  source_rect: &mut Rectangle,
+  dest_rect: &mut Rectangle,
+  target: &mut RenderTexture,
 ) {
   *screen_width = get_screen_width();
   *screen_height = get_screen_height();
