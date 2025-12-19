50e26c5c42ee5763a6fcaac5860d7b9033aca838
MBATheGamer <mbathegamer@gmail.com>
2025-12-19 15:32:39 +0100

    Add an example

---
+++ src/bin/72_vector_angle.rs
@@ -0,0 +1,123 @@
+fn main() {
+  const SCREEN_WIDTH: i32 = 800;
+  const SCREEN_HEIGHT: i32 = 450;
+
+  init_window(
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    "raylib [shapes] example - vector angle",
+  );
+
+  let v0 = Vector2 {
+    x: SCREEN_WIDTH as f32 / 2.0,
+    y: SCREEN_HEIGHT as f32 / 2.0,
+  };
+  let v1 = v0 + Vector2 { x: 100.0, y: 80.0 };
+
+  let angle = 0.0;
+  let angle_mode = false;
+
+  set_target_fps(60);
+
+  while !window_should_close() {
+    let startangle = if !angle_mode {
+      (-v0.line_angle(v1)).to_degrees()
+    } else {
+      0.0
+    };
+
+    let v2 = get_mouse_position();
+
+    if is_key_pressed(KeyboardKey::KeySpace) {
+      angle_mode = !angle_mode;
+    }
+
+    if !angle_mode && is_mouse_button_down(MouseButton::Right) {
+      v1 = get_mouse_position();
+    }
+
+    if !angle_mode {
+      let v1_normal = (v1 - v0).normalize();
+      let v2_normal = (v2 - v0).normalize();
+
+      angle = v1_normal.angle(v2_normal).to_degrees();
+    } else if angle_mode {
+      angle = v0.line_angle(v2).to_degrees();
+    }
+
+    begin_drawing();
+
+    clear_background(colors::RAYWHITE);
+
+    if !angle_mode {
+      draw_text("MODE 0: Angle between V1 and V2", 10, 10, 20, colors::BLACK);
+      draw_text("Right Click to Move V2", 10, 30, 20, colors::DARKGRAY);
+
+      draw_line_ex(v0, v1, 2.0, colors::BLACK);
+      draw_line_ex(v0, v2, 2.0, colors::RED);
+
+      draw_circle_sector(
+        v0,
+        40.0,
+        startangle,
+        startangle + angle,
+        32,
+        fade(colors::GREEN, 0.6),
+      );
+    } else if angle_mode {
+      draw_text(
+        "MODE 1: Angle formed by line V1 to V2",
+        10,
+        10,
+        20,
+        colors::BLACK,
+      );
+
+      draw_line(
+        0,
+        SCREEN_HEIGHT / 2,
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT / 2,
+        colors::LIGHTGRAY,
+      );
+      draw_line_ex(v0, v2, 2.0, colors::RED);
+
+      draw_circle_sector(
+        v0,
+        40.0,
+        startangle,
+        startangle - angle,
+        32,
+        fade(colors::GREEN, 0.6),
+      );
+    }
+
+    draw_text("v0", v0.x as i32, v0.y as i32, 10, colors::DARKGRAY);
+
+    if !angle_mode && (v0 - v1).y > 0.0 {
+      draw_text("v1", v1.x as i32, v1.y as i32 - 10, 10, colors::DARKGRAY);
+    }
+    if !angle_mode && (v0 - v1).y < 0.0 {
+      draw_text("v1", v1.x as i32, v1.y as i32, 10, colors::DARKGRAY);
+    }
+
+    if angle_mode {
+      draw_text("v1", v0.x as i32 + 40, v0.y as i32, 10, colors::DARKGRAY);
+    }
+
+    draw_text(
+      "v2",
+      v2.x as i32 - 10,
+      v2.y as i32 - 10,
+      10,
+      colors::DARKGRAY,
+    );
+
+    draw_text("Press SPACE to change MODE", 460, 10, 20, colors::DARKGRAY);
+    draw_text(&format!("ANGLE: {:2.2}", angle), 10, 70, 20, colors::LIME);
+
+    end_drawing();
+  }
+
+  close_window();
+}
