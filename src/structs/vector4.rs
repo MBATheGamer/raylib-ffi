c9a4992af3a2fec8c6362c24d9bfe1736894fcfa
MBATheGamer <mbathegamer@gmail.com>
2025-12-23 16:19:28 +0100

    Add `zero` method to `Vector4` struct

--- src/structs/vector4.rs
+++ src/structs/vector4.rs
@@ -5,3 +5,15 @@ pub struct Vector4 {
   pub z: f32,
   pub w: f32,
 }
+
+impl Vector4 {
+  #[inline]
+  pub fn zero() -> Vector4 {
+    return Vector4 {
+      x: 0.0,
+      y: 0.0,
+      z: 0.0,
+      w: 0.0,
+    };
+  }
+}
