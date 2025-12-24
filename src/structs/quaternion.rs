d537a648424d0dfe1a1fe7fa95b800f66cf1ec56
MBATheGamer <mbathegamer@gmail.com>
2025-12-24 16:15:12 +0100

    Add `add` function to `Quaternion` struct

--- src/structs/quaternion.rs
+++ src/structs/quaternion.rs
@@ -1,6 +1,20 @@
+#[derive(Clone, Copy)]
 pub struct Quaternion {
   pub x: f32,
   pub y: f32,
   pub z: f32,
   pub w: f32,
+}
+
+impl Quaternion {
+  // Add two quaternions
+  #[inline]
+  pub fn add(self, q2: Quaternion) -> Quaternion {
+    return Quaternion {
+      x: self.x + q2.x,
+      y: self.y + q2.y,
+      z: self.z + q2.z,
+      w: self.w + q2.w,
+    };
+  }
 }
