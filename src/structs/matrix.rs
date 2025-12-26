73140432fb269479809ed4c20004bd8b0682c228
MBATheGamer <mbathegamer@gmail.com>
2025-12-26 16:40:56 +0100

    Add `determinant` method to `Matrix` struct

--- src/structs/matrix.rs
+++ src/structs/matrix.rs
@@ -18,3 +18,38 @@ pub struct Matrix {
   pub m11: f32,
   pub m15: f32, // Matrix fourth row (4 components)
 }
+
+impl Matrix {
+  // Compute matrix determinant
+  #[inline]
+  pub fn determinant(self) -> f32 {
+    // Cache the matrix values (speed optimization)
+    let (a00, a01, a02, a03) = (self.m0, self.m1, self.m2, self.m3);
+    let (a10, a11, a12, a13) = (self.m4, self.m5, self.m6, self.m7);
+    let (a20, a21, a22, a23) = (self.m8, self.m9, self.m10, self.m11);
+    let (a30, a31, a32, a33) = (self.m12, self.m13, self.m14, self.m15);
+
+    return a30 * a21 * a12 * a03 - a20 * a31 * a12 * a03 - a30 * a11 * a22 * a03
+      + a10 * a31 * a22 * a03
+      + a20 * a11 * a32 * a03
+      - a10 * a21 * a32 * a03
+      - a30 * a21 * a02 * a13
+      + a20 * a31 * a02 * a13
+      + a30 * a01 * a22 * a13
+      - a00 * a31 * a22 * a13
+      - a20 * a01 * a32 * a13
+      + a00 * a21 * a32 * a13
+      + a30 * a11 * a02 * a23
+      - a10 * a31 * a02 * a23
+      - a30 * a01 * a12 * a23
+      + a00 * a31 * a12 * a23
+      + a10 * a01 * a32 * a23
+      - a00 * a11 * a32 * a23
+      - a20 * a11 * a02 * a33
+      + a10 * a21 * a02 * a33
+      + a20 * a01 * a12 * a33
+      - a00 * a21 * a12 * a33
+      - a10 * a01 * a22 * a33
+      + a00 * a11 * a22 * a33;
+  }
+}
