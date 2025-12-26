e0a3be928fe1738fe62e99ec57eda7a220bce461
MBATheGamer <mbathegamer@gmail.com>
2025-12-26 16:45:34 +0100

    Add `scale` method to `Matrix` struct

--- src/structs/matrix.rs
+++ src/structs/matrix.rs
@@ -323,4 +323,27 @@ impl Matrix {
       m15: 1.0,
     };
   }
+
+  // Get scaling matrix
+  #[inline]
+  pub fn scale(x: f32, y: f32, z: f32) -> Matrix {
+    return Matrix {
+      m0: x,
+      m4: 0.0,
+      m8: 0.0,
+      m12: 0.0,
+      m1: 0.0,
+      m5: y,
+      m9: 0.0,
+      m13: 0.0,
+      m2: 0.0,
+      m6: 0.0,
+      m10: z,
+      m14: 0.0,
+      m3: 0.0,
+      m7: 0.0,
+      m11: 0.0,
+      m15: 1.0,
+    };
+  }
 }
