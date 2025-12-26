2a0c8912acc9b29d75270533f2357683429a332c
MBATheGamer <mbathegamer@gmail.com>
2025-12-26 20:39:06 +0100

    Complete example

--- src/bin/74_kaleidoscope.rs
+++ src/bin/74_kaleidoscope.rs
@@ -1,3 +1,19 @@
+use std::sync::{LazyLock, Mutex};
+
+use raylib_ffi::{
+  consts::colors,
+  core::{
+    begin_drawing, begin_mode_2d, clear_background, close_window, end_drawing, end_mode_2d,
+    init_window,
+    mouse::{get_mouse_position, is_mouse_button_down},
+    set_target_fps, window_should_close,
+  },
+  enums::MouseButton,
+  shape::draw_line_ex,
+  structs::{Camera2D, Vector2},
+  text::{draw_fps, draw_text},
+};
+
 const MAX_DRAW_LINES: usize = 8192;
 
 struct Line {
@@ -22,7 +38,7 @@ fn main() {
   let symmetry = 6;
   let angle = 360.0 / symmetry as f32;
   let thickness = 3.0;
-  let mouse_pos = Vector2::zero();
+  let mut mouse_pos = Vector2::zero();
   let scale_vector = Vector2::new(1.0, -1.0);
   let offset = Vector2::new(SCREEN_WIDTH as f32 / 2.0, SCREEN_HEIGHT as f32 / 2.0);
 
@@ -36,15 +52,15 @@ fn main() {
   set_target_fps(20);
 
   while !window_should_close() {
-    let lines = LINES.lock().expect("Expecting LINES unlocked");
+    let mut lines = LINES.lock().expect("Expecting LINES unlocked");
     let prev_mouse_pos = mouse_pos;
     mouse_pos = get_mouse_position();
 
-    let line_start = mouse_pos - offset;
-    let line_end = prev_mouse_pos - offset;
+    let mut line_start = mouse_pos - offset;
+    let mut line_end = prev_mouse_pos - offset;
 
     if is_mouse_button_down(MouseButton::Left) {
-      let s = 0;
+      let mut s = 0;
 
       while s < symmetry && lines.len() < (MAX_DRAW_LINES - 1) {
         line_start = line_start.rotate(angle.to_radians());
@@ -70,7 +86,7 @@ fn main() {
 
     begin_mode_2d(camera);
     for _ in 0..symmetry {
-      let i = 0;
+      let mut i = 0;
       while i < lines.len() {
         draw_line_ex(lines[i].start, lines[i].end, thickness, colors::BLACK);
         draw_line_ex(
