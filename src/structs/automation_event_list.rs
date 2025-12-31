910eb30e8204407d20cca5e0948969999cbc10ee
MBATheGamer <mbathegamer@gmail.com>
2025-12-31 11:02:03 +0100

    Add `get` method to `AutomationEventList` struct

--- src/structs/automation_event_list.rs
+++ src/structs/automation_event_list.rs
@@ -7,3 +7,13 @@ pub struct AutomationEventList {
   pub count: u32,               // Events entries count
   events: *mut AutomationEvent, // Events entries
 }
+
+impl AutomationEventList {
+  pub fn get(&self, index: usize) -> Option<&AutomationEvent> {
+    if index >= self.count as usize {
+      return None;
+    }
+
+    return unsafe { Some(&*self.events.add(index)) };
+  }
+}
