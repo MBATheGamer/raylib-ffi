cc28bb7671a5634c7ab0b7866af5cee1e16f06e9
MBATheGamer <mbathegamer@gmail.com>
2025-12-31 10:59:20 +0100

    Add `get` method to `FilePathList` struct

--- src/structs/file_path_list.rs
+++ src/structs/file_path_list.rs
@@ -1,7 +1,28 @@
+use std::ffi::CStr;
+
 #[repr(C)]
 #[derive(Clone, Copy, Default)]
 pub struct FilePathList {
   pub capacity: u32,
   pub count: u32,
-  pub paths: *mut *mut i8,
+  paths: *mut *mut i8,
+}
+
+impl FilePathList {
+  pub fn get(&self, index: usize) -> Option<&str> {
+    if index >= self.count as usize {
+      return None;
+    }
+
+    let ptr = unsafe { *self.paths.add(index) };
+
+    if ptr.is_null() {
+      return None;
+    }
+
+    return match unsafe { CStr::from_ptr(ptr).to_str() } {
+      Ok(path) => Some(path),
+      Err(_) => None,
+    };
+  }
 }
