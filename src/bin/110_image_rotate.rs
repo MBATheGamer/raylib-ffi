cfec9b5204731488156e66ac1eff82ff95f6b0a5
MBATheGamer <mbathegamer@gmail.com>
2026-01-04 20:11:16 +0100

    Complete example

--- src/bin/110_image_rotate.rs
+++ src/bin/110_image_rotate.rs
@@ -1,3 +1,15 @@
+use raylib_ffi::{
+  consts::colors,
+  core::{
+    begin_drawing, clear_background, close_window, end_drawing, init_window,
+    keyboard::is_key_pressed, mouse::is_mouse_button_pressed, set_target_fps, window_should_close,
+  },
+  enums::{KeyboardKey, MouseButton},
+  structs::Texture,
+  text::draw_text,
+  texture::{draw_texture, image_rotate, load_image, load_texture_from_image, unload_texture},
+};
+
 fn main() {
   const SCREEN_WIDTH: i32 = 800;
   const SCREEN_HEIGHT: i32 = 450;
@@ -8,13 +20,13 @@ fn main() {
     "raylib [textures] example - image rotate",
   );
 
-  let image45 = load_image("resources/raylib_logo.png");
-  let image90 = load_image("resources/raylib_logo.png");
-  let image_neg90 = load_image("resources/raylib_logo.png");
+  let mut image45 = load_image("resources/raylib_logo.png");
+  let mut image90 = load_image("resources/raylib_logo.png");
+  let mut image_neg90 = load_image("resources/raylib_logo.png");
 
-  image_rotate(&image45, 45);
-  image_rotate(&image90, 90);
-  image_rotate(&image_neg90, -90);
+  image_rotate(&mut image45, 45);
+  image_rotate(&mut image90, 90);
+  image_rotate(&mut image_neg90, -90);
 
   let textures: Vec<Texture> = vec![
     load_texture_from_image(image45),
@@ -22,7 +34,7 @@ fn main() {
     load_texture_from_image(image_neg90),
   ];
 
-  let current_texture = 0;
+  let mut current_texture = 0;
 
   set_target_fps(60);
 
